76b48007e3fcbcbbd2dc938e98fdf1f3def129bf
znk1007 <1032479745@qq.com>
2017-02-28 13:33:55 +0800

    0.0.1

--- ZNKPlayer/ZNKPlayerFramework/ZNKPlayerClasses/ZNKPlayer/ZNKPlayer.h
+++ ZNKPlayer/ZNKPlayerFramework/ZNKPlayerClasses/ZNKPlayer/ZNKPlayer.h
@@ -15,6 +15,19 @@ typedef NS_ENUM(NSInteger, ZNKMPMovieScalingMode) {
     ZNKMPMovieScalingModeFill        /**非均匀缩放。按可是边界渲染*/
 };
 
+@interface ZNKTool : NSObject
+/**单例模式*/
++ (ZNKTool *)sharedInstance:(BOOL)kill;
+/**获取设备旋转方向的通知,即使关闭了自动旋转,一样可以监测到设备的旋转方向*/
++ (void)startNotifyDeviceOrientation;
+/**停止监听设备旋转*/
++ (void)endNotifyDeviceOrientation;
+/**类清除缓存*/
+- (void)cleanCache;
+/**获取旋转方向*/
+- (void)getDeviceOrientation:(void(^)(UIInterfaceOrientation orientation))completionHandler;
+@end
+
 @interface ZNKPlayer : ZNKBaseView
 /**单例设计模式*/
 + (ZNKPlayer *)sharedManager:(BOOL)kill;
